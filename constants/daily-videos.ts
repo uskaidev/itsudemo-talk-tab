8bac7641501eb9789439c39ba96b645274763fdb
uskaidev <yusuke.koike@newh.co.jp>
2025-02-18 00:24:27 +0900

    Restore today's video feature

---
+++ constants/daily-videos.ts
@@ -0,0 +1,45 @@
+export const DAILY_VIDEOS: Record<string, {
+  videoId: string;
+  title: string;
+}> = {
+  "02-14": {
+    videoId: "46ctPFDeaLM",
+    title: "2月14日の動画"
+  },
+  "02-15": {
+    videoId: "STj-a53wfzo",
+    title: "2月15日の動画"
+  },
+  "02-16": {
+    videoId: "isc07eC1Oqo",
+    title: "2月16日の動画"
+  },
+  "02-17": {
+    videoId: "rF_n2Czlsvo",
+    title: "2月17日の動画"
+  },
+  "02-18": {
+    videoId: "suHuC-lTkvY",
+    title: "2月18日の動画"
+  },
+  "02-19": {
+    videoId: "FVgyb6WL0ZE",
+    title: "2月19日の動画"
+  },
+  "02-20": {
+    videoId: "bBfJrmwhpyg",
+    title: "2月20日の動画"
+  },
+  "02-21": {
+    videoId: "xPzFJy69fEU",
+    title: "2月21日の動画"
+  },
+  "02-22": {
+    videoId: "QtAhAY5G_vs",
+    title: "2月22日の動画"
+  },
+  "02-23": {
+    videoId: "bbFC-YQ8xzk",
+    title: "2月23日の動画"
+  }
+};
